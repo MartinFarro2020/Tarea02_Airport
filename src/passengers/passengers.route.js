1c486a7e373771c8e0a28b33b4825ff118c4364b
martinfarro <martin.farro.martinez@gmail.com>
2023-10-09 23:58:22 -0500

    primer commit

---
+++ src/passengers/passengers.route.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import {
+  findAllPassengers,
+  createPassenger,
+  findOnePassenger,
+  updatePassenger,
+  deletePassenger,
+} from "./passengers.controller.js";
+
+export const router = Router();
+
+router
+  .route("/")
+  .get(findAllPassengers)
+  .post(createPassenger);
+
+router
+  .route("/:id")
+  .get(findOnePassenger)
+  .patch(updatePassenger)
+  .delete(deletePassenger)
